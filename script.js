5b3e8f5b7a9260841ade45585a8987b8d6c0993d
JeanneEricksonCooley <cool0020@gmail.com>
2015-04-29 17:56:56 -0500

    addition search functionality

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 var i;
+var j;
 var gameArray = [];
 
 var apikey = '10d0a85137d94904c75e456dcdf91468227c09ca'; // Put your API key here
 // Use this function to do stuff with your results.
 // It is called after 'search' is executed.
 
+//search("batman");
+
+
 
 function searchCallback(results) {
-    for(i=0; i < 8; i++) {
-        //get results, add to array j(push?)
-        gameArray.push(results[i].name, results[i].image.thumb_url, results[i].deck, results[i].platforms.name);
-        //console.log(results[i]);
-        console.log(gameArray);
-    }
+    $(".bob").empty();
+    gameArray = results;
+        $(".displayGames").append("<div class='bob'><h1>" + gameArray[0].name + "</h1><br><img src='" +     gameArray[0].image.thumb_url + "'/><br><p>" + gameArray[0].deck + "<br>" + gameArray[0].platforms[0].name + "</p></div>");
 
 }
 
+
+//function displayStuff(results) {
+
+//}
 
 
 
@@ -23,12 +28,16 @@ function searchCallback(results) {
 
 $(document).ready(function() {
 // Start the search here!
-    $(".searchButton").on("click", function(){
+
        //take in the user input, call search function
-    });
+        $(".container").on("click", ".searchButton", function() {
+            array = [];
+            var searchVal = $(".searchField").val();
+            console.log(searchVal);
+            search(searchVal);
+        });
 });
 
-search("batman");
 
 // HELPER FUNCTION
 // Executes a search using 'query' and runs searchCallback on the results of a success.
